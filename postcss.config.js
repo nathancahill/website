7172edb349675a0cb0215b923fced083750e2649
Nathan Cahill <nathan@nathancahill.com>
2020-04-01 14:04:07 +0200

    add missing files

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable global-require */
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./src/**/*.html', './src/**/*.svelte'],
+    content: ['./src/**/*.html', './src/**/*.svelte', './src/**/*.svexy'],
 
-    whitelistPatterns: [/svelte-/, /ml-4/],
+    whitelistPatterns: [/svelte-/],
 
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })

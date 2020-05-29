0e4a493341f39640d35565359089f24bc45b26bd
Nathan Cahill <nathan@nathancahill.com>
2020-05-29 13:02:27 +0200

    update tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,17 +1,10 @@
 /* global cy */
-describe('Sapper template app', () => {
+describe('index', () => {
     beforeEach(() => {
         cy.visit('/')
     })
 
-    it('has the correct <h1>', () => {
-        cy.contains('h1', 'Hello world')
-    })
-
-    it('navigates to /about', () => {
-        cy.get('nav a')
-            .contains('about')
-            .click()
-        cy.url().should('include', '/about')
+    it('renders title in <h1>', () => {
+        cy.contains('h1', 'Nathan Cahill')
     })
 })

c5ca9f6afb835f96539e04905b3ad78c4ba270fd
Nate <nate@Salmans-MBP.lan>
2021-02-11 09:13:58 +0100

    rework metadata

--- src/routes/_post.svelte
+++ src/routes/_post.svelte
@@ -1,9 +1,17 @@
 <script>
     export let title
+    export let description
+    export let path
 </script>
 
 <svelte:head>
     <title>{title}</title>
+    <meta name="description" content="{description}" />
+	<meta name="twitter:card" value="summary" />
+	<meta property="og:title" content="{title}" />
+	<meta property="og:type" content="article" />
+	<meta property="og:description" content="{description}" />
+    <meta property="og:url" content="https://nathancahill.com{path}" />
 </svelte:head>
 
 <article class="markdown max-w-2xl mx-auto">

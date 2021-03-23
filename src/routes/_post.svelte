77cc388010e725f0c9b32ab193e2121df9bf5297
Nathan Cahill <nathan@nathancahill.com>
2021-03-23 12:01:15 +0100

    add og:image

--- src/routes/_post.svelte
+++ src/routes/_post.svelte
@@ -7,11 +7,12 @@
 <svelte:head>
     <title>{title}</title>
     <meta name="description" content="{description}" />
-	<meta name="twitter:card" value="summary" />
-	<meta property="og:title" content="{title}" />
-	<meta property="og:type" content="article" />
-	<meta property="og:description" content="{description}" />
+    <meta name="twitter:card" value="summary" />
+    <meta property="og:title" content="{title}" />
+    <meta property="og:type" content="article" />
+    <meta property="og:description" content="{description}" />
     <meta property="og:url" content="https://nathancahill.com{path}" />
+    <meta property="og:image" content="https://nathancahill.com{path}.png" />
 </svelte:head>
 
 <article class="markdown max-w-2xl mx-auto">
